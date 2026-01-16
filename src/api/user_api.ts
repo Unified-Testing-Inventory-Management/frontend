41e9e4c04eb4f5f32ea75bab572688277d5488f1
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-16 16:06:56 +0800

    feat: Implement user API functions for authentication and registration

---
+++ src/api/user_api.ts
@@ -0,0 +1,18 @@
+import type { TLoginUserData, TRegisterUserData, User } from "@/@types";
+import { api } from "./axios_api";
+
+
+export const checkAuth = async (): Promise<User> => {
+    const res = await api.get<User>("/api/v1/users/auth/me");
+    return res.data;
+};
+
+export const registerUser = async (data: TRegisterUserData) => {
+    const res = await api.post("/api/v1/users/auth/register", data);
+    return res.data;
+}
+
+export const loginUser = async (data: TLoginUserData) => {
+    const res = await api.post("/api/v1/users/auth/login", data);
+    return res.data;
+}

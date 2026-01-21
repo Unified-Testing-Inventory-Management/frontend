2b835357fc84712d866aba948fedd3b0ca1c2aa6
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-21 11:05:36 +0800

    chore: Comment out TanStackDevtools in root route for future reference

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,4 @@
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-
-import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 
 import type { QueryClient } from '@tanstack/react-query'
 
@@ -14,18 +10,6 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
       <Outlet />
-      <TanStackDevtools
-        config={{
-          position: 'bottom-right',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-          TanStackQueryDevtools,
-        ]}
-      />
     </>
   ),
 })

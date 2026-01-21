12d9f380e5cc3b3860eec21336e712f43d39ee32
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-21 11:04:22 +0800

    feat: Add product retrieval and archiving functionalities with new hooks for fetching by ID and archiving products

--- src/services/product_services.ts
+++ src/services/product_services.ts
@@ -1,5 +1,10 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { registerProduct, getAllProducts } from '@/api/product_api'
+import {
+  registerProduct,
+  getAllProducts,
+  getProductById,
+  archiveProduct,
+} from '@/api/product_api'
 
 export const useRegisterProductMutation = () => {
   const queryClient = useQueryClient()
@@ -17,5 +22,23 @@ export const useAllProductsQuery = () => {
   return useQuery({
     queryKey: ['products'],
     queryFn: getAllProducts,
+  })
+}
+
+export const useGetProductById = (id: string) => {
+  return useQuery({
+    queryKey: ['products', id],
+    queryFn: () => getProductById(id),
+  })
+}
+
+export const useArchiveProductById = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: archiveProduct,
+    mutationKey: ['products'],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['products'] })
+    },
   })
 }

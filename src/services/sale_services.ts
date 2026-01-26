06a7566aa73f1b941de3f053ee947cefe39f7838
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-26 14:31:28 +0800

    feat: Add useTransactionProduct hook for handling transaction mutations and automatic query invalidation

--- src/services/sale_services.ts
+++ src/services/sale_services.ts
@@ -1,9 +1,20 @@
-import { useQuery } from "@tanstack/react-query";
-import { allProductSales } from "@/api/sale_api";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { allProductSales, transactionProducts } from "@/api/sale_api";
 
 export const useAllProductSalesQuery = () => {
     return useQuery({
         queryFn: allProductSales,
         queryKey: ["transactions"],
+    })
+}
+
+export const useTransactionProduct = () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+        mutationFn: transactionProducts,
+        mutationKey: ["transactions"],
+        onSuccess: ()=> {
+            queryClient.invalidateQueries({queryKey: ["transactions"]})
+        }
     })
 }

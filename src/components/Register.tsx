a842360aa7361f7b35674883604f5bd7d1c79be5
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-02-02 21:52:07 +0800

    feat: Enhance registration form with password visibility toggle and validation for confirmPassword

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -11,6 +11,7 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import type { TRegisterUserData } from '@/@types'
 import { useRegisterUserMutation } from '@/services/user_services'
+import { Eye, EyeClosed } from 'lucide-react'
 
 interface RegisterProps {
   open: boolean
@@ -18,19 +19,30 @@ interface RegisterProps {
 }
 
 export function Register({ open, onOpenChange }: RegisterProps) {
+  const [isShowPassword, setIsShowPassword] = useState(false)
+  const [isShowConfirmPassword, setShowConfirmPassword] = useState(false)
+  const [message, setMessage] = useState("");
   const [formData, setFormData] = useState<TRegisterUserData>({
     firstName: "",
     lastName: "",
     username: "",
     password: "",
+    confirmPassword: "",
     role: "Admin"
   })
   const register = useRegisterUserMutation();
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
-
+    let isError = false
     console.log('Register:', formData)
+
+    if (formData.confirmPassword !== formData.password) {
+      setMessage("Password does not match")
+      isError = true
+    }
+
+    if (isError) return true
 
     register.mutate(formData, {
       onSuccess: () => {
@@ -40,6 +52,7 @@ export function Register({ open, onOpenChange }: RegisterProps) {
           lastName: "",
           username: "",
           password: "",
+          confirmPassword: "",
           role: ""
         });
       },
@@ -107,17 +120,45 @@ export function Register({ open, onOpenChange }: RegisterProps) {
               placeholder="Choose a username"
             />
           </div>
-          <div className="space-y-2">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              id="password"
-              name="password"
-              type="password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-              placeholder="Create a password"
-            />
+          <div className='relative'>
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                name="password"
+                type={isShowPassword ? "text" : "password"}
+                value={formData.password}
+                onChange={handleChange}
+                required
+                placeholder="Create a password"
+              />
+              {formData.password.length > 0 && (
+                isShowPassword ? <Eye onClick={() => setIsShowPassword(false)} className='text-gray-300 text-sm absolute top-7 right-2.5' /> :
+                  <EyeClosed onClick={() => setIsShowPassword(true)} className='text-gray-300 text-sm absolute top-7 right-2.5' />
+              )
+              }
+            </div>
+          </div>
+          <div className='relative'>
+            <div className="space-y-2">
+              <Label htmlFor="confirmPassword">Confirm Password</Label>
+              <Input
+                id="confirmPassword"
+                name="confirmPassword"
+                type={isShowConfirmPassword ? "text" : "password"}
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                required
+                placeholder="Confirm your password"
+                className={`${message ? "ring-2 ring-red-500" : ""}`}
+              />
+              {formData.confirmPassword.length > 0 && (
+                isShowConfirmPassword ? <Eye onClick={() => setShowConfirmPassword(false)} className='text-gray-300 text-sm absolute top-7 right-2.5' /> :
+                  <EyeClosed onClick={() => setShowConfirmPassword(true)} className='text-gray-300 text-sm absolute top-7 right-2.5' />
+              )
+              }
+              {message && <span className='text-sm -mt-2 text-red-500'>{message}</span>}
+            </div>
           </div>
           <div className="flex justify-end gap-2 pt-4">
             <Button

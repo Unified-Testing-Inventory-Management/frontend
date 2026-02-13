f3a1ba0f2ed40ad6cb88810c7b254c1d374d04bc
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-02-13 20:34:29 +0800

    refactor: Update user API endpoints to remove versioning for consistency

--- src/api/user_api.ts
+++ src/api/user_api.ts
@@ -2,26 +2,26 @@ import type { TLoginUserData, TRegisterUserData, TUpdateUserData, User } from '@
 import { api } from './axios_api'
 
 export const checkAuth = async (): Promise<User> => {
-    const res = await api.get<User>('/api/v1/users/auth/me')
+    const res = await api.get<User>('/users/auth/me')
     return res.data
 }
 
 export const registerUser = async (data: Omit<TRegisterUserData, "confirmPassword">) => {
-    const res = await api.post('/api/v1/users/auth/register', data)
+    const res = await api.post('/users/auth/register', data)
     return res.data
 }
 
 export const loginUser = async (data: TLoginUserData) => {
-    const res = await api.post('/api/v1/users/auth/login', data)
+    const res = await api.post('/users/auth/login', data)
     return res.data
 }
 
 export const updateUser = async (data: TUpdateUserData) => {
-    const res = await api.patch("/api/v1/users/auth/update-profile", data)
+    const res = await api.patch("/users/auth/update-profile", data)
     return res.data
 }
 
 export const logoutUser = async () => {
-    const res = await api.post('/api/v1/users/auth/logout')
+    const res = await api.post('/users/auth/logout')
     return res.data
 }
